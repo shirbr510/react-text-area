813919f29f2d3eac9086cda39ecbdd9557f37f11
Shir Brass <shirbr510@gmail.com>
2017-11-09 18:30:39 +0200

    add placeholders to stories

--- src/TextArea.stories.js
+++ src/TextArea.stories.js
@@ -6,23 +6,25 @@ import { action } from '@storybook/addon-actions';
 
 const maxRows=5;
 
+const placeholder="Placeholder";
+
 const onChangeActionLogger=action('input changed!');
 
 storiesOf('TextArea', module)
     .add('uncontrolled empty example',()=>{
-        return <TextArea maxRows={maxRows} onChange={onChangeActionLogger}/>;
+        return <TextArea placeholder={placeholder} maxRows={maxRows} onChange={onChangeActionLogger}/>;
     })
     .add('first example', () =>{
         const value ='1 - this component has a min and max number of rows.';
-        return <TextArea maxRows={maxRows} onChange={onChangeActionLogger} value={value}/>;
+        return <TextArea placeholder={placeholder} maxRows={maxRows} onChange={onChangeActionLogger} value={value}/>;
     })
     .add('second example', () =>{
         const value ='2 - if the user has not yet reached the max number of rows an empty line will always be provided at the bottom';
-        return <TextArea maxRows={maxRows} onChange={onChangeActionLogger} value={value}/>;
+        return <TextArea placeholder={placeholder} maxRows={maxRows} onChange={onChangeActionLogger} value={value}/>;
     })
     .add('third example', () =>{
         const value ='3 - Line breaks should work when hitting “Enter”, and word breaks to new line';
-        return <TextArea maxRows={maxRows} onChange={onChangeActionLogger} value={value}/>;
+        return <TextArea placeholder={placeholder} maxRows={maxRows} onChange={onChangeActionLogger} value={value}/>;
     });
 
 
