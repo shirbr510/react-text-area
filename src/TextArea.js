f4ce006f30324fb9f11f047c2a7c427a41c02385
Shir Brass <shirbr510@gmail.com>
2017-11-09 18:06:23 +0200

    implemented the basic stories and some HTML5 TextArea

--- src/TextArea.js
+++ src/TextArea.js
@@ -2,10 +2,39 @@
 
 import React, {Component} from 'react';
 
+type Value = string| number;
+
+type Props={
+    placeholder?:string,
+    value?:Value,
+    minRows?:number,
+    maxRows:number,
+    onChange?:(event:InputEvent,value:Value) => any
+}
+
 class TextArea extends Component {
 
+    static defaultProps={
+        minRows:1,
+        //default is noop
+        onChange:()=>{}
+    };
+
+    props:Props;
+
+    constructor(props:Props){
+        super(props);
+        this._onChange=this._onChange.bind(this);
+    }
+
+    _onChange=(e:InputEvent)=>{
+        const {onChange} = this.props;
+        onChange(e.target.value);
+    };
+
     render() {
-        return <div/>;
+        const {placeholder,value,maxRows} = this.props;
+        return <textarea placeholder={placeholder} wrap="soft" value={value} onInput={this._onChange} rows={maxRows}/>;
     }
 }
 
--- src/TextArea.stories.js
+++ src/TextArea.stories.js
@@ -3,7 +3,28 @@ import TextArea from './TextArea'
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+
+const maxRows=5;
+
+const onChangeActionLogger=action('input changed!');
 
 storiesOf('TextArea', module)
-    .add('1 - this component has a min and max number of rows.', () => <TextArea/>);
+    .add('uncontrolled empty example',()=>{
+        return <TextArea maxRows={maxRows} onChange={onChangeActionLogger}/>;
+    })
+    .add('first example', () =>{
+        const value ='1 - this component has a min and max number of rows.';
+        return <TextArea maxRows={maxRows} onChange={onChangeActionLogger} value={value}/>;
+    })
+    .add('second example', () =>{
+        const value ='2 - if the user has not yet reached the max number of rows an empty line will always be provided at the bottom';
+        return <TextArea maxRows={maxRows} onChange={onChangeActionLogger} value={value}/>;
+    })
+    .add('third example', () =>{
+        const value ='3 - Line breaks should work when hitting “Enter”, and word breaks to new line';
+        return <TextArea maxRows={maxRows} onChange={onChangeActionLogger} value={value}/>;
+    });
+
+
+
+
