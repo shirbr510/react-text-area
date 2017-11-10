edc8e2d2ccfc6f293a1b0a7d8cc2af1106442d94
Shir Brass <shirbr510@gmail.com>
2017-11-10 16:50:04 +0200

    replaced eventlistener hook with "onKeyDown" *noisy commit due to reformatting of file*

--- src/TextArea.js
+++ src/TextArea.js
@@ -3,58 +3,61 @@
 import './textarea.less'
 import React, {Component} from 'react';
 
-type Value = string| number;
+type Value = string | number;
 
-type Props={
-    placeholder?:string,
-    value?:Value,
-    minRows?:number,
-    maxRows:number,
-    onChange?:(event:InputEvent,value:Value) => any,
+type Props = {
+    placeholder?: string,
+    value?: Value,
+    minRows?: number,
+    maxRows: number,
+    onChange?: (event: InputEvent, value: Value) => any,
 }
 
 class TextArea extends Component {
 
-    static defaultProps={
-        minRows:1,
+    static defaultProps = {
+        minRows: 1,
         //default is noop
-        onChange:()=>{}
+        onChange: () => {
+        }
     };
 
-    textArea:HTMLTextAreaElement;
+    textArea: HTMLTextAreaElement;
 
-    props:Props;
+    props: Props;
 
-    constructor(props:Props){
+    constructor(props: Props) {
         super(props);
-        this._onChange=this._onChange.bind(this);
-        this._autoresize=this._autoresize.bind(this);
+        this._onChange = this._onChange.bind(this);
+        this._autoResize = this._autoResize.bind(this);
     }
 
-    componentDidMount(){
-        this.textArea.addEventListener('keydown', this._autoresize);
-    }
-
-    componentWillunmount(){
-        this.textArea.removeEventListener('keydown', this._autoresize);
-    }
-
-    _autoresize(){
+    _autoResize(e: KeyboardEvent) {
         const {textArea} = this;
-        setTimeout(()=>{
+        setTimeout(() => {
             textArea.style.cssText = 'height:auto; padding:0';
             textArea.style.cssText = `height: ${textArea.scrollHeight}px`;
-        },0);
+        }, 0);
     }
 
-    _onChange(e:InputEvent){
+    _onChange(e: InputEvent) {
         const {onChange} = this.props;
         onChange(e.target.value);
     }
 
     render() {
-        const {placeholder,value,maxRows} = this.props;
-        return <textarea ref={c=>this.textArea=c} className='react-text-area' placeholder={placeholder} wrap="soft" value={value} onInput={this._onChange} rows={maxRows}/>;
+        const {placeholder, value, maxRows} = this.props;
+        return (
+            <textarea
+            ref={c => this.textArea = c}
+            className='react-text-area'
+            placeholder={placeholder}
+            value={value}
+            wrap="soft"
+            rows={maxRows}
+            onKeyDown={this._autoResize}
+            onInput={this._onChange}/>
+        );
     }
 }
 
